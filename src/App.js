944c3f2b17d99fdc6218ac997355fb235deeb716
Anant <anantsinghraghuvanshi1@gmail.com>
2022-03-24 21:12:58 +0530

    Changes done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+import Join from "./components/Join/Join";
+
+const ENDPOINT = "http://localhost:4500/";
+
 
 function App() {
+
+  
+
   return (
     <div className="App">
+
+    <Router>
+      <Route exact path= "/" component ={Join} />
+
+      <Route path = "/chat" />
+
+
+    </Router>
       
     </div>
   );
