19952efc1a1aed9ecc9f21f6f0b4739d7ae37535
Anant <anantsinghraghuvanshi1@gmail.com>
2022-03-24 23:52:32 +0530

    latest changes done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter, Router, Route} from 'react-router-dom';
 import './App.css';
 import Join from "./components/Join/Join";
+import Chat from "./components/Chat/Chat"
 
 const ENDPOINT = "http://localhost:4500/";
 
 
 function App() {
 
-  
-
-  return (
-    <div className="App">
-
-    <Router>
-      <Route exact path= "/" component ={Join} />
-
-      <Route path = "/chat" />
+return (
 
 
-    </Router>
-      
-    </div>
-  );
+  <Router>
+
+    <Route exact path="/" component={Join} />
+    <Route path="/chat" component={Chat} />
+
+  </Router>
+
+);
 }
 
 export default App;
--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Join.css";
+import logo from "../../images/logo.png";
+import {Link} from "react-router-dom";
+
+let user;
+
+const sendUser = () => {
+  user = document.getElementById('joinInput').value;
+  document.getElementById('joinInput').value = " ";
+}
 
 const Join = () => {
+
+
+  const [name, setName] = useState(" ");
+
+
+
   return (
-    <div>
-        <h1>Join Page</h1>
+    <div className ="JoinPage">
+        <div className = "JoinContainer">
+        <img src={logo} alt="logo" />
+          <h1>Infinity Chat</h1>
+          <input onChange= {(e)=> setName(e.target.value)} placeholder='Enter Your Name' type="text" id= "joinInput"></input>
+      <Link to= "/chat">  </Link>    <button onClick= {sendUser} className= "JoinBtn ">Login</button>
+        </div>
     </div>
   )
 }
 
-export default Join
+export default Join;
+export {user};
