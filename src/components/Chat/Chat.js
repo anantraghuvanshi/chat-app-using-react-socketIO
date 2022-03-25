4eecb35f4c76a3f3881f2712fd62226842ec31a0
Anant <anantsinghraghuvanshi1@gmail.com>
2022-03-25 23:18:05 +0530

    files updated

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -3,10 +3,15 @@ import {user} from "../Join/Join"
 
 const Chat = () => {
   return (
-    <div>
+    <div classname ="ChatPage">
+    <div className="ChatContainer">
+    <div className="header"> </div>
+    <div className="chatBox"> </div>
+    <div className="inputBox"> </div>
+    </div>
     <h1>{user}</h1>
     </div>
   )
 }
 
-export default Chat
+export default Chat;
--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -24,7 +24,7 @@ const Join = () => {
           type="text"
           id="joinInput"
         ></input>
-        <Link onChange={(e)=>e.target.value===""?e.preventDefault():null} to="/chat"> </Link>{" "}
+        <Link onChange={(e)=>!name ? e.preventDefault():null} to="/chat"> </Link>{" "}
         <button onClick={sendUser} className="JoinBtn ">
           Login
         </button>
