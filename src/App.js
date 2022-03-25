d0e3981a85e8ec5e997439791b99095d08a0fbc7
Anant <anantsinghraghuvanshi1@gmail.com>
2022-03-25 23:14:27 +0530

    files updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import './App.css';
-import {BrowserRouter, Router, Route} from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter, Router, Route } from "react-router-dom";
 import Join from "./components/Join/Join";
-import Chat from "./components/Chat/Chat"
+import Chat from "./components/Chat/Chat";
 
 const ENDPOINT = "http://localhost:4500/";
 
-
 function App() {
-
-return (
-
-
-  <Router>
-
-    <Route exact path="/" component={Join} />
-    <Route path="/chat" component={Chat} />
-
-  </Router>
-
-);
+  return (
+    <Router>
+      <Route exact path="/" component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -1,33 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import "./Join.css";
 import logo from "../../images/logo.png";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 let user;
 
 const sendUser = () => {
-  user = document.getElementById('joinInput').value;
-  document.getElementById('joinInput').value = " ";
-}
+  user = document.getElementById("joinInput").value;
+  document.getElementById("joinInput").value = " ";
+};
 
 const Join = () => {
-
-
   const [name, setName] = useState(" ");
 
-
-
   return (
-    <div className ="JoinPage">
-        <div className = "JoinContainer">
+    <div className="JoinPage">
+      <div className="JoinContainer">
         <img src={logo} alt="logo" />
-          <h1>Infinity Chat</h1>
-          <input onChange= {(e)=> setName(e.target.value)} placeholder='Enter Your Name' type="text" id= "joinInput"></input>
-      <Link to= "/chat">  </Link>    <button onClick= {sendUser} className= "JoinBtn ">Login</button>
-        </div>
+        <h1>Infinity Chat</h1>
+        <input
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Enter Your Name"
+          type="text"
+          id="joinInput"
+        ></input>
+        <Link onChange={(e)=>e.target.value===""?e.preventDefault():null} to="/chat"> </Link>{" "}
+        <button onClick={sendUser} className="JoinBtn ">
+          Login
+        </button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Join;
-export {user};
+export { user };
