02e277b68dccb3a0cc0028fdf9caaaaaf5d0b60e
Anant <anantsinghraghuvanshi1@gmail.com>
2022-03-29 23:03:47 +0530

    chat update

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -4,19 +4,20 @@ import socketIO from "socket.io-client";
 import "./Chat.css";
 import sendLogo from "../../images/send.png";
 import Message from "../Message/Message";
+import ReactScrollToBottom from "react-scroll-to-bottom";
 
 const ENDPOINT = "http://localhost:4500/";
 
 let socket;
 
 const Chat = () => {
-  const [id, setId] = useState("")
-  const send = ( )=>{
-    const message =document.getElementById('chatInput').value;
-    socket.emit('message',{message, id});
-    document.getElementById('chatInput').value='';
-  }
-  
+  const [id, setId] = useState("");
+  const [messages, setMessages] = useState([])
+  const send = () => {
+    const message = document.getElementById("chatInput").value;
+    socket.emit("message", { message, id });
+    document.getElementById("chatInput").value = "";
+  };
 
   useEffect(() => {
     socket = socketIO(ENDPOINT, { transports: ["websocket"] });
@@ -25,47 +26,52 @@ const Chat = () => {
       alert("Connected");
       setId(socket.id);
     });
+    console.log(socket);
     socket.emit("joined", { user });
 
     socket.on("welcome", (data) => {
+      setMessages([...messages, data]);
       console.log(data.user, data.message);
     });
 
     socket.on("userJoined", (data) => {
+      setMessages([...messages, data]);
       console.log(data.user, data.message);
     });
 
-    socket.on('leave',(data) => {
+    socket.on("leave", (data) => {
+      setMessages([...messages, data]);
       console.log(data.user, data.message);
-    } )
+    });
 
     return () => {
-      socket.emit("disconnect");
+      socket.disconnect();
       socket.off();
     };
   }, []);
 
   useEffect(() => {
-    socket.on('sendMessage',(data)=>{
+    socket.on("sendMessage", (data) => {
+      setMessages([...messages, data]);
       console.log(data.user, data.message);
-    })
-  
+    });
+
     return () => {
-      
+      socket.off();
     }
-  }, [])
-  
+    ;
+  }, [messages]);
 
   return (
     <div className="chatPage">
       <div className="chatContainer">
         <div className="header"> </div>
-        <div className="chatBox"> 
-        <Message />
-        </div>
+        <ReactScrollToBottom className="chatBox">
+        {messages.map((item,i)=> {<Message message={item.message}/>})}
+        </ReactScrollToBottom>
         <div className="inputBox">
           <input type="text" id="chatInput" />
-          <button onClick ={send} className="sendBtn">
+          <button onClick={send} className="sendBtn">
             <img src={sendLogo} alt="Send" />
           </button>
         </div>
